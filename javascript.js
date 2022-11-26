15554075ebba7b73888ad3257636512a2a282266
PrinceJetro <adegbuyijephthah@gmail.com>
2022-11-26 18:39:02 +0100

    changed js

--- javascript.js
+++ javascript.js
@@ -9,6 +9,7 @@
   }
 
   $(document).ready(function(){
+   
     $(".more_projects_button").click(function(){
       $(".hidden").fadeToggle();
       $(".hidden").css("display","flex");
@@ -16,3 +17,33 @@
     })
  
   });
+ 
+$(document).ready(function(){
+  hmm()
+})
+  let career=["Junior Full Stack Developer ","Senior Software Engineer"];
+  let index =0;
+  job=0
+ 
+  function hmm(){
+    
+   
+    document.getElementById("upcoming").innerHTML=`${career[job].slice(0 ,index)} `;
+   
+    index= index +1;
+    if(index > career[job].length){
+      job = job+1;
+      index= 0;
+      }
+      if(job === career.length){
+        job=0;
+      }
+
+    setTimeout(() => {
+      hmm()
+    }, 300);
+    
+}
+
+
+
